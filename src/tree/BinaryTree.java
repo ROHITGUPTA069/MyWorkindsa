8f408482f9e29cb51a4d2845e5c210cb27bd7bba
Rohit Gupta <rohitgupta5tbaksa@gmail.com>
2025-07-19 18:46:34 +0530

    tree done

---
+++ src/tree/BinaryTree.java
@@ -0,0 +1,26 @@
+package tree;
+
+import java.util.*;
+
+public class BinarytTree {
+
+    BinarytTree(){
+
+    }
+
+    class Node{
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val){
+            this.val = val;
+        }
+    }
+
+    private Node root;
+    //Insert elements
+    public void populate(Scanner scanner){
+        
+    }
+}
